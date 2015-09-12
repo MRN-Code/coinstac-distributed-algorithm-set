3b20d72fadbed10d5fadf6cb39d5bb93a3a864ba
Dylan Wood <dwood@mrn.org>
2015-09-12 14:30:22 -0700

    use jStat for distribution sampling

--- example.js
+++ example.js
@@ -1,3 +1,5 @@
+'use strict';
+
 global.coinslog = function(arg) {
     return typeof arg === 'object' ? console.dir(arg) : console.log(arg);
 };
@@ -15,30 +17,35 @@ var dummyROIs = [
 ];
 var response = [3, 1, 2, 1.2];
 var regressor = [1, 1, 1];
-coinslog('oneShotRegression.objective');
-coinslog(oneShotRegression.objective(regressor, dummyROIs, response));
+global.coinslog('oneShotRegression.objective');
+global.coinslog(oneShotRegression.objective(regressor, dummyROIs, response));
 
 var minimized = n.uncmin(function(x) {
     return oneShotRegression.objective(x, dummyROIs, response);
 }, regressor).solution;
 
-coinslog('minimized oneShotRegression.objective');
-coinslog(minimized);
-coinslog('re-applied oneShotRegression.objective');
-coinslog(oneShotRegression.objective(minimized, dummyROIs, response));
+global.coinslog('minimized oneShotRegression.objective');
+global.coinslog(minimized);
+global.coinslog('re-applied oneShotRegression.objective');
+global.coinslog(oneShotRegression.objective(minimized, dummyROIs, response));
 
-coinslog('Laplace Noise with scale of 1');
-coinslog(laplace.noise(1));
+global.coinslog('Laplace Noise with scale of 1');
+global.coinslog(laplace.noise(1));
 
-coinslog('Laplace Noise with scale of 100');
-coinslog(laplace.noise(100));
+global.coinslog('Laplace Noise with scale of 100');
+global.coinslog(laplace.noise(100));
 
-coinslog('Laplace Noise with scale of 1000');
-coinslog(laplace.noise(1000));
+global.coinslog('Laplace Noise with scale of 1000');
+global.coinslog(laplace.noise(1000));
 
+global.coinslog('Inverse Laplace Transform of 0.5');
+global.coinslog(laplace.inverse(0.5));
 
-coinslog('Inverse Laplace Transform of 0');
-coinslog(laplace.inverse(0,1,0));
+global.coinslog('Inverse Laplace Transform of 0.3');
+global.coinslog(laplace.inverse(0.3));
 
-coinslog('Inverse Laplace Transform of 0.2');
-coinslog(laplace.inverse(0.2,1,0));
+global.coinslog('Inverse Laplace Transform of 0.9');
+global.coinslog(laplace.inverse(0.9));
+
+global.coinslog('Inverse Laplace Transform of 0.9, mu = 2, b  = 2');
+global.coinslog(laplace.inverse(0.9, 2, 2));
