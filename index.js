5ab07e42ea133b024fe8439da7be4d1eb7fc9748
Dylan Wood <dwood@mrn.org>
2015-10-15 21:38:10 -0700

    rename one-shot-regression to ridge-regression, and rename vars to match coinstac nomenclature

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports.oneShotRegression = require('./lib/one-shot-regression.js');
+module.exports.ridgeRegression = require('./lib/ridgeRegression.js');
 module.exports.laplace = require('./lib/laplace.js');
 module.exports.utils = require('./lib/utils.js');
